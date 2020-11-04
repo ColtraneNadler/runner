96f46d15cbe2a4d7ba4116589150c49b5a9de616
lagauche <ian.bercov.ross@gmail.com>
2020-11-04 04:24:20 -0500

    temporarily disabling environment 3

--- js/game.js
+++ js/game.js
@@ -87,7 +87,7 @@ let envs = [
 	['/assets/Enviroment2Packaged.glb', InitCityEnv, SetUpStaticCityEnv, CitySpawnTypes],
 	['/assets/Enviroment33forIan.glb', InitForestEnv, SetUpStaticForestEnv, ForestSpawnTypes]
 ]
-let randomSceneIdx = Math.floor(3 * Math.random());
+let randomSceneIdx = Math.floor(2 * Math.random());
 let envController = new EnvController(envs[randomSceneIdx][1], envs[randomSceneIdx][2], envs[randomSceneIdx][3], 13, 10);
 loader.load(envs[randomSceneIdx][0], function (glb) {
 	envController.Init(glb);
