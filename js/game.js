c1ae1d301114ccae665bbb4312a832194abf67b3
coltrane <coltranetunes@gmail.com>
2020-11-18 16:14:57 -0700

    updates

--- js/game.js
+++ js/game.js
@@ -719,3 +719,13 @@ function HardResetAnimsToIdle() {
 	}
 }
 
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
